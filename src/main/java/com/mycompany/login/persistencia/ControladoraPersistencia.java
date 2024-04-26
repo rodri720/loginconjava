3eabdf4f2e998b3026aab8e19ff0863de06b9310
rodri720 <tomiyrami@gamil.com>
2024-04-26 19:41:07 -0300

    persistencia

---
+++ src/main/java/com/mycompany/login/persistencia/ControladoraPersistencia.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.login.persistencia;
+
+import com.mycompany.login.logica.Usuario;
+import java.util.List;
+
+
+public class ControladoraPersistencia {
+    
+    UsuarioJpaController usuJpa = new UsuarioJpaController();
+
+    public List<Usuario> traerUsuarios(){
+        return usuJpa.findUsuarioEntities();
+    }
+    
+    
+    
+}
